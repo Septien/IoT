4afe255c74bbfdb44275faa09593d79402e515c3
Jose Septien <jaseptienh@gmail.com>
2019-07-10 17:28:50 -0500

    Aquire data from sensors.

--- raspberry/main.py
+++ raspberry/main.py
@@ -3,6 +3,7 @@ Main module for the raspberry.
 """
 
 import csv
+from csv import writer
 import time
 import threading as t
 import dht22
@@ -21,16 +22,21 @@ def writeToFile(fileName, data):
         data_writer.write_row(data)
 
 def main():
+    user = False
+    saveDHTData = True
+    hclogFile = "hclog.csv";
+    dhtlogFile = "dhtlog.csv"
     # Get the sensors
     dht22S = dht22.DHT22()
     hcsr04S = hcsr04.HCSR04()
     ledS = led.LED()
-    pb = push_button.PushButton(dht22S, ledS)
+    resetPB = push_button.PushButton(dht22S, ledS, 11)
     # Create the queues for each sensor
     qDatadht = q.Queue(1000)
     qdhtExit = q.Queue(1)
     qDataHC = q.Queue(1000)
     qhcExit = q.Queue(1)
+    # Create locks
     dhtLock = t.Lock()
     hcLock = t.Lock()
     dhtExitL = t.Lock()
@@ -44,12 +50,38 @@ def main():
     thread2.start()
 
     endProcess = False
-    
+
     while not endProcess:
+        dht22Data = None
+        # Is there data from DHT22?
         dhtLock.acquire()
         if not qDatadht.empty():
-            data = qDatadht.get()
+            dht22Data = qDatadht.get()
         dhtLock.release()
+        # Is there data from HC-SR04?
+        hcData = None
+        hcLock.acquire()
+        if not qDataHC.empty():
+            hcData = qDataHC.get()
+        hcLock.release()
+        # Was there a presence?
+        if hcData:
+            pTime = time.asctime()
+            data = [pTime, hcData]
+            if user:
+                pass
+            else:
+                ledS.turnOnLED();
+                writeToFile(hclogFile, data)
+        # DHT data
+        if saveDHTData:
+            if dht22Data:
+                if user:
+                    pass
+                else:
+                    writeToFile(dhtlog, dht22Data)
+        # Get params
+
 
 if __name__ == '__main__':
     main()
