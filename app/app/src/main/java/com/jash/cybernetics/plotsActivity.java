8a32d5a54cbd1f3307fa2dd738761f237f72dafd
Jose Septien <jaseptienh@gmail.com>
2019-07-31 15:09:58 -0500

    Add activity for plots.

---
+++ app/app/src/main/java/com/jash/cybernetics/plotsActivity.java
@@ -0,0 +1,14 @@
+package com.jash.cybernetics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class plotsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plots);
+    }
+}
