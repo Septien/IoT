536718d873eb99978afec084f679119144322680
Jose Septien <jaseptienh@gmail.com>
2019-08-01 17:16:48 -0500

    Add the client thread.

--- app/app/src/main/java/com/jash/cybernetics/MainActivity.java
+++ app/app/src/main/java/com/jash/cybernetics/MainActivity.java
@@ -4,26 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.View;
-
-// For internet connection
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static BlockingDeque<String> q;
+    private Thread clientThread = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        q = new LinkedBlockingDeque<String>();
+        clientThread = new client("127.0.0.1", 8080, q);
+        clientThread.run();
     }
 
     public void btnOnClickPlts(View view) {
