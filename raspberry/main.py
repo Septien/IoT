e1aab1496253680ca7faf54d77f52ae35f997e13
Jose Septien <jaseptienh@gmail.com>
2019-08-01 12:21:40 -0500

    Add server, write only to file sensor data, and modify parameters.

--- raspberry/main.py
+++ raspberry/main.py
@@ -13,6 +13,7 @@ import sThread
 import led
 import push_button
 import queue as Q
+import server
 
 
 def writeToFile(fileName, data):
@@ -32,31 +33,36 @@ def main():
     connectWiFi()
     user = False
     saveDHTData = True
-    hclogFile = "hclog.csv";
-    dhtlogFile = "dhtlog.csv"
+    logFile = "logger.txt"
     # Get the sensors
     dht22S = dht22.DHT22()
     hcsr04S = hcsr04.HCSR04()
     ledS = led.LED()
     resetPB = push_button.PushButton(dht22S, ledS, 11)
-    btButton = push_button.PushButtonBT(12)
     # Create the queues for each sensor
     qDatadht = q.Queue(1000)
     qdhtExit = q.Queue(1)
     qDataHC = q.Queue(1000)
     qhcExit = q.Queue(1)
+    serverQ = q.Queue(100)
+    sQExit = q.Queue(1)
     # Create locks
     dhtLock = t.Lock()
     hcLock = t.Lock()
     dhtExitL = t.Lock()
     hcExitL = t.Lock()
+    serverLock = t.Lock()
+    sExitL = t.Lock()
     # Create the thread for DHT sensor
     thread1 = sThread.SensorThread(1, "Thread1", qDatadht, dhtLock, qdhtExit, dhtExitL)
     # Create thread for HC-SR04 sensor
     thread2 = sThread.SensorThread(2, "Thread2", qDataHC, hcLock, qhcExit, hcExitL)
+    # Thread for the server
+    serverT = server.ServerS(3, "Server", logFile, serverQ, sQExit, sExitL)
     # Start
     thread1.start()
     thread2.start()
+    serverT.start()
 
     endProcess = False
 
@@ -76,21 +82,26 @@ def main():
         # Was there a presence?
         if hcData:
             pTime = time.asctime()
-            data = [pTime, hcData]
-            if user:
-                pass
-            else:
-                ledS.turnOnLED();
-                writeToFile(hclogFile, data)
+            data = ["HC", hcData, pTime]
+            ledS.turnOnLED();
+            writeToFile(logFile, data)
         # DHT data
         if saveDHTData:
             if dht22Data:
-                if user:
-                    pass
-                else:
-                    writeToFile(dhtlog, dht22Data)
-        # Get params
+                data = ["DHT", dht22Data[0], dht22Data[1]]
+                writeToFile(logFile, data)
 
+        # Get params
+        serverLock.acquire()
+        if not serverQ.empty():
+            data = serverQ.get()
+        serverLock.release()
+        if data[0] == "1":
+            dht22S.setDefaults()
+            self.led.turnOffLED()
+        dht22S.setSamplingInterval(int(data[1]), int(data[2]))
+        dht22S.setPSampling(int(data[3]))
+        saveDHTData = bool(int(data[4]))
 
 if __name__ == '__main__':
     main()
