0d57d14bfe042f55e45a3f20c607707430408aad
Jose <jaseptienh@gmail.com>
2019-08-19 14:10:59 -0500

    Correc typo and change pin.

--- raspberry/push_button.py
+++ raspberry/push_button.py
@@ -6,10 +6,12 @@ class PushButton:
     def __init__(self, dht22, led, pin):
         self.dht22 = dht22
         self.led = led
-        gpio.setmode(gpio.BOARD)
-        gpio.setup(pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+        self.pin = pin
+        gpio.setwarnings(False)
+        gpio.setmode(gpio.BCM)
+        gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
         # Set callback
-        gpio.add_event_detect(11, gpio.RISING, callback=self.button_callback, bouncetime=10)
+        gpio.add_event_detect(self.pin, gpio.RISING, callback=self.button_callback, bouncetime=10)
 
     def button_callback(channel):
         """
