a2ee81379868b374ed0f63a223c70dedf098638d
Jose Septien <jaseptienh@gmail.com>
2019-08-26 17:27:21 -0500

    Add class for time picker.

--- app/app/src/main/java/com/jash/cybernetics/TimePickerFragment.java
+++ app/app/src/main/java/com/jash/cybernetics/TimePickerFragment.java
@@ -1,4 +1,51 @@
 package com.jash.cybernetics;
 
-public class TimePickerFragment {
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+// Create a time picker
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+    private int hour;
+    private int minute;
+    private Context context;
+    TimePickerFragment(Context context) {
+        this.context = context;
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minuteN) {
+        Calendar c = Calendar.getInstance();
+        if (hourOfDay < c.get(Calendar.HOUR_OF_DAY) || minuteN < c.get(Calendar.MINUTE)) {
+            Toast alert = Toast.makeText(context, "Seleccione una hora igual o posterior a la actual", Toast.LENGTH_SHORT);
+            alert.show();
+            return;
+        }
+        minute = minuteN;
+        hour = hourOfDay;
+    }
+
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getHour() {
+        return hour;
+    }
 }
